175ab57dd4e875ae0a2a8e7ba4101e6abfe580a9
christopherfelt <chris.h.felt@gmail.com>
2020-10-25 16:29:16 -0600

    finished task backend

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -6,3 +6,14 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
+
+    def update(self, instance, validated_data):
+        instance.board_id = validated_data.get("board_id", instance.board_id)
+        instance.list_id = validated_data.get("list_id", instance.list_id)
+        instance.body = validated_data.get("body", instance.body)
+        instance.creatorEmail = validated_data.get(
+            "creatorEmail", instance.creatorEmail
+        )
+        instance.save()
+
+        return instance
